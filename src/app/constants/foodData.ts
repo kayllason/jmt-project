dd4d33e34a888ac7249699f84a41e40cf0fe2fe6
kayllason <kaylla.app@gmail.com>
2024-12-19 16:40:54 +0900

    add: mock data

---
+++ src/app/constants/foodData.ts
@@ -0,0 +1,24 @@
+const mockData = [
+  { id: 'food-001', name: '김치찌개', imageURL: '', category: '한식' },
+  { id: 'food-002', name: '불고기', imageURL: '', category: '한식' },
+  { id: 'food-003', name: '비빔밥', imageURL: '', category: '한식' },
+  { id: 'food-004', name: '떡볶이', imageURL: '', category: '분식' },
+  { id: 'food-005', name: '갈비찜', imageURL: '', category: '한식' },
+  { id: 'food-006', name: '삼계탕', imageURL: '', category: '한식' },
+  { id: 'food-007', name: '냉면', imageURL: '', category: '한식' },
+  { id: 'food-008', name: '김밥', imageURL: '', category: '분식' },
+  { id: 'food-009', name: '칼국수', imageURL: '', category: '한식' },
+  { id: 'food-010', name: '쌀국수', imageURL: '', category: '기타' },
+  { id: 'food-011', name: '돈가스', imageURL: '', category: '일식' },
+  { id: 'food-012', name: '스테이크', imageURL: '', category: '양식' },
+  { id: 'food-013', name: '파스타', imageURL: '', category: '양식' },
+  { id: 'food-014', name: '초밥', imageURL: '', category: '일식' },
+  { id: 'food-015', name: '라멘', imageURL: '', category: '일식' },
+  { id: 'food-016', name: '짜장면', imageURL: '', category: '중식' },
+  { id: 'food-017', name: '딤섬', imageURL: '', category: '중식' },
+  { id: 'food-018', name: '피자', imageURL: '', category: '양식' },
+  { id: 'food-019', name: '치킨', imageURL: '', category: '한식' },
+  { id: 'food-020', name: '마라탕', imageURL: '', category: '중식' },
+];
+
+const category = ['양식', '한식', '일식', '중식', '간편식', '디저트', '분식', '기타'];

612b2d27bf406c53eaefb77913987ef3c455998a
kayllason <kaylla.app@gmail.com>
2024-12-22 20:58:20 +0900

    feat: interface FoodData export해서 다른 컴포넌트에서도 사용할 수 있게 수정

--- src/app/components/Card.tsx
+++ src/app/components/Card.tsx
@@ -2,7 +2,7 @@
 import React from 'react';
 import Image from 'next/image';
 
-interface FoodData {
+export interface FoodData {
   id: number;
   name: string;
   imageURL: string;
@@ -17,7 +17,14 @@ interface CardProps {
 export default function Card({ foodData }: CardProps) {
   return (
     <div className="relative w-80 h-80 border-2 border-gray-800 rounded-lg overflow-hidden">
-      <Image src={foodData.imageURL ? foodData.imageURL : '/images/pizza.jpg'} alt="FoodImage" sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"  priority={true} fill className="object-cover" />
+      <Image
+        src={foodData.imageURL ? foodData.imageURL : '/images/pizza.jpg'}
+        alt="FoodImage"
+        sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+        priority={true}
+        fill
+        className="object-cover"
+      />
       <div className="absolute bottom-0 bg-black bg-opacity-50 text-white text-center w-full py-1">{foodData.name}</div>
     </div>
   );

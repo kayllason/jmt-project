e5c23a853998f2c3393fd7b38134ed6356b7d377
shfkd123 <shfkd123@gmail.com>
2024-12-22 20:00:56 +0900

    object-cover tailwind css 적용

--- src/app/components/Card.tsx
+++ src/app/components/Card.tsx
@@ -17,7 +17,7 @@ interface CardProps {
 export default function Card({ foodData }: CardProps) {
   return (
     <div className="relative w-80 h-80 border-2 border-gray-800 rounded-lg overflow-hidden">
-      <Image src={foodData.imageURL ? foodData.imageURL : '/images/pizza.jpg'} alt="FoodImage" sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"  priority={true} fill style={{ objectFit: 'cover' }} />
+      <Image src={foodData.imageURL ? foodData.imageURL : '/images/pizza.jpg'} alt="FoodImage" sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"  priority={true} fill className="object-cover" />
       <div className="absolute bottom-0 bg-black bg-opacity-50 text-white text-center w-full py-1">{foodData.name}</div>
     </div>
   );

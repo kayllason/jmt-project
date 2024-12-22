2075535195f23fc3ad8a5ac6a2f56e995352f350
shfkd123 <shfkd123@gmail.com>
2024-12-22 16:33:40 +0900

    체크된 카테고리의 상태값을 List 컴포넌트에서 관리 하고 props로 전달받아 선택된 카테코리 상태값 변경

--- src/app/components/Category.tsx
+++ src/app/components/Category.tsx
@@ -1,67 +1,82 @@
 'use client'
 import React, { useState, useEffect } from 'react';
-import { category } from './../constants/foodData';
 
+interface CategoryData {
+  id: number;
+  categoryId: string;
+  name: string;
+}
 
-export default function Category() {
-    const [isAllChecked, setIsAllChecked] = useState(false);
-    const [checkedItems, setCheckedItems] = useState<string[]>([]);
+interface CategoryProps {
+  checkedItems: string[];
+  setCheckedItems: React.Dispatch<React.SetStateAction<string[]>>;
+}
 
-    const handleAllCheck = (event: React.ChangeEvent<HTMLInputElement>) => {
-        const checked = event.target.checked;
-        setIsAllChecked(checked);
-        setCheckedItems(checked ? [...category] : []);
-    };
+export default function Category({ checkedItems, setCheckedItems }: CategoryProps) {
+  const [isAllChecked, setIsAllChecked] = useState<boolean>(false);
+  const [categoryData, setCategoryData] = useState<CategoryData[]>([]);
 
-    const handleCheck = (event: React.ChangeEvent<HTMLInputElement>) => {
-        const { value, checked } = event.target;
-        if (checked) {
-        setCheckedItems((prev) => [...prev, value]);
-        } else {
-        setCheckedItems((prev) => prev.filter((item) => item !== value));
-        }
-    };
+  const handleAllCheck = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const checked = event.target.checked;
+    setIsAllChecked(checked);
+    setCheckedItems(checked ? categoryData.map((category) => category.categoryId) : []);
+  };
+
+  const handleCheck = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { value, checked } = event.target;
+    if (checked) {
+      setCheckedItems((prev) => [...prev, value]);
+    } else {
+      setCheckedItems((prev) => prev.filter((item) => item !== value));
+    }
+  };
 
   useEffect(() => {
-    setIsAllChecked(checkedItems.length === category.length);
-  }, [checkedItems]);
+    fetch(`/data/category.json`)
+      .then((res) => res.json())
+      .then((data) => {
+        setCategoryData(data.data);
+      });
+  }, []);
+
+  useEffect(() => {
+    setIsAllChecked(checkedItems.length === categoryData.length);
+  }, [checkedItems, categoryData]);
 
   return (
     <>
       <ul className="items-center w-full text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg sm:flex dark:text-white">
         <li className="w-full border-b border-gray-200 sm:border-b-0 sm:border-r">
-            <div className="flex items-center ps-3">
-                <input
-                id="all-checkbox"
-                type="checkbox"
-                checked={isAllChecked}
-                onChange={handleAllCheck}
-                className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
-                />
-                <label htmlFor="all-checkbox" className="w-full py-3 ms-2 text-sm font-medium text-gray-900">
-                전체
-                </label>
-             </div>
+          <div className="flex items-center ps-3">
+            <input
+              id="all-checkbox"
+              type="checkbox"
+              checked={isAllChecked}
+              onChange={handleAllCheck}
+              className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
+            />
+            <label htmlFor="all-checkbox" className="w-full py-3 ms-2 text-sm font-medium text-gray-900">
+              전체
+            </label>
+          </div>
         </li>
-        {category.map((cat, index) => {
-          return (
-            <li key={`cat${index}`} className="w-full border-b border-gray-200 sm:border-b-0 sm:border-r">
-              <div className="flex items-center ps-3">
-                <input
-                  id={`category${index}`}
-                  type="checkbox"
-                  value={cat}
-                  checked={checkedItems.includes(cat)}
-                  onChange={handleCheck}
-                  className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
-                />
-                <label htmlFor={`category${index}`} className="w-full py-3 ms-2 text-sm font-medium text-gray-900">
-                  {cat}
-                </label>
-              </div>
-            </li>
-          );
-        })}
+        {categoryData.map((category) => (
+          <li key={category.id} className="w-full border-b border-gray-200 sm:border-b-0 sm:border-r">
+            <div className="flex items-center ps-3">
+              <input
+                id={`category${category.id}`}
+                type="checkbox"
+                value={category.categoryId}
+                checked={checkedItems.includes(category.categoryId)}
+                onChange={handleCheck}
+                className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
+              />
+              <label htmlFor={`category${category.id}`} className="w-full py-3 ms-2 text-sm font-medium text-gray-900">
+                {category.name}
+              </label>
+            </div>
+          </li>
+        ))}
       </ul>
     </>
   );
